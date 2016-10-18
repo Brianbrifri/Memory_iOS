f02d985500f161a23bfd9a21b19748788575387e
Brian Koehler <brianbrifri@gmail.com>
2016-10-17 19:32:54 -0500

    Initial commit

---
+++ Memory_BK/Memory_BKTests/Memory_BKTests.swift
@@ -0,0 +1,36 @@
+//
+//  Memory_BKTests.swift
+//  Memory_BKTests
+//
+//  Created by vm mac on 10/16/16.
+//
+//
+
+import XCTest
+@testable import Memory_BK
+
+class Memory_BKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
