b4a01b2fb48ae4b6971e3372e5ba133df375fd53
Brian Koehler <brianbrifri@gmail.com>
2016-10-26 22:20:46 -0500

    Front and back views

--- Memory_BK/Memory_BK/BackView.swift
+++ Memory_BK/Memory_BK/BackView.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class BackView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var emojiLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(emojiLabel)
+        
+        emojiLabel.translatesAutoresizingMaskIntoConstraints = false
+        emojiLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        emojiLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        emojiLabel.textAlignment = .center
+        emojiLabel.font = UIFont.systemFont(ofSize: 48)
+        emojiLabel.text = "👻" // default text for noticing if a view didn't load the card data right
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
--- Memory_BK/Memory_BK/GameControllerCollectionViewCell.swift
+++ Memory_BK/Memory_BK/GameControllerCollectionViewCell.swift
@@ -12,14 +12,18 @@ class GameControllerCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var cellCardView: UIView!
     
     var frontView: FrontView!
+    var backView: BackView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         cellCardView.backgroundColor = UIColor.green
         frontView = FrontView(frame: frame)
+        backView = BackView(frame: frame)
         
         cellCardView.addSubview(frontView)
+        cellCardView.addSubview(backView)
         frontView.backgroundColor = UIColor.red
+        backView.backgroundColor = UIColor.darkGray
     }
     
 }
