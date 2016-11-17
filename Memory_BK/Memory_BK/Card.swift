938473ec55fc530ffc4f6e654139eb0e381e1acf
Brian Koehler <brianbrifri@gmail.com>
2016-11-16 20:56:30 -0600

    removed commented out code

--- Memory_BK/Memory_BK/Card.swift
+++ Memory_BK/Memory_BK/Card.swift
@@ -2,26 +2,7 @@ import UIKit
 
 class Card: UIView {
     
-    var ID: Int = -1
-    
-    var frontIsShowing: Bool = false
-    var notMatched: Bool = true
-    private let BACKTAG = 100
-    private let FRONTTAG = 200
-    
-    //Make these optional if adding subviews from GameController
-//    var frontView = CardFrontView(frame: CGRect.zero)
-//    var backView = CardBackView(frame: CGRect.zero)
-//    
-//    
-    //Get rid of this method if add subviews from
-    //GameController
-//    override func draw(_ rect: CGRect) {
-//        addSubview(frontView)
-//        addSubview(backView)
-//        frontView.isHidden = true
-//    }
-//    
+   
     override func layoutSubviews() {
         backgroundColor = UIColor.darkGray
         super.layoutSubviews()
@@ -44,32 +25,4 @@ class Card: UIView {
             subView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         }
     }
-    
-    //MARK: Getter and setter for ID
-    func setID(ID: Int) {
-        self.ID = ID
-    }
-    
-    func getID() -> Int {
-        return ID
-    }
-
-    //MARK: Flip function
-    //Duration: 1 second, changes which side it flips from, animates the views showing and hiding
-//    func flip() {
-//        UIView.transition(with: self, duration: 1, options: frontIsShowing ? .transitionFlipFromLeft : .transitionFlipFromRight, animations: {
-//            //flip views
-//            self.frontView.isHidden = !self.frontIsShowing
-//            self.backView.isHidden = self.frontIsShowing
-//            self.frontIsShowing = !self.frontIsShowing
-//            print(" ")
-//            print("frontView showing: \(self.frontView.isHidden)")
-//            print("backView showing: \(self.backView.isHidden)")
-//
-//            }) { (true) in
-//            //more stuff
-////            print("On Completion")
-//        
-//        }
-//    }
 }
